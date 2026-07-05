abedc0309a2bc45ff62fc485d7084dc1fe7735aa
Harpreet Kaur <8055kaur@gmail.com>
2026-07-05 12:02:29 +0530

    Enhance Flipkart scraper to include images and better data
    Updated the scraper to include image extraction and improved data handling.

--- scrapers/flipkart_scraper.py
+++ scrapers/flipkart_scraper.py
@@ -1,31 +1,49 @@
+
+
+
 from bs4 import BeautifulSoup
-import pandas as pd
 import requests
-url = "https://www.flipkart.com/search?q=shoes&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_2_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_2_0_na_na_na&as-pos=2&as-type=TRENDING&suggestionId=shoes&requestId=137bfca7-eb1c-4aa8-8ce9-79a525bf0eb2"
-response = requests.get(url)
-headers ={
-    'user-Agent': 'Mozilla/5.0(Windows NT 10.0; Win64; v64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+
+url = "https://www.flipkart.com/search?q=shoes&as=on&as-show=on"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
 }
 
 def scrape_flipkart_data():
 
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    soup.prettify()
-    shoes = soup.find_all('div', class_= 'nZIRY7')
-    shoes_data  = []
-    for i in shoes:
-    
-        title = i.find("a", class_="atJtCj Qum9aC")     
-        price = i.find("div", class_="hZ3P6w")         
-        discount = i.find("div", class_= "HQe8jr")      
-        size = i.find("div", class_= "yZLfgx")        
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    shoes = soup.find_all("div", class_="nZIRY7")
+
+    shoes_data = []
+
+    for shoe in shoes:
+
+        image = shoe.find("img")
+
+        title = shoe.find("a", class_="atJtCj Qum9aC")
+
+        price = shoe.find("div", class_="hZ3P6w")
+
+        discount = shoe.find("div", class_="HQe8jr")
+
+        size = shoe.find("div", class_="yZLfgx")
 
         shoes_data.append({
-        "title":title,
-        "price":price,
-        "discount" : discount,
-        "size":size
-    })
+
+            "image": image["src"] if image else None,
+
+            "title": title.get_text(strip=True) if title else "N/A",
+
+            "price": price.get_text(strip=True) if price else "N/A",
+
+            "discount": discount.get_text(strip=True) if discount else "N/A",
+
+            "size": size.get_text(strip=True) if size else "N/A"
+
+        })
 
     return shoes_data
